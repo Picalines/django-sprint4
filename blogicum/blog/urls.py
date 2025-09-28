c784e824f77afffb4c2f85fc91d7f8a5268336e7
picalines <picalines0@gmail.com>
2025-09-28 23:09:19 +0300

    feat: /profile/<username>

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import index, post_detail, category_posts
-
+from .views import ProfileDetailView, category_posts, index, post_detail
 
 app_name = 'blog'
 
 
 urlpatterns = [
+    path(
+        'profile/<slug:username>/', ProfileDetailView.as_view(), name='profile'
+    ),
     path('posts/<int:post_id>/', post_detail, name='post_detail'),
     path(
         'category/<slug:category_slug>/', category_posts, name='category_posts'
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,7 @@
 from core.constants import POSTS_BY_PAGE
+from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView
 
 from .models import Category, Post
 
@@ -7,6 +9,14 @@ from .models import Category, Post
 def index(request):
     post_list = Post.filter_visible(Post.objects)[:POSTS_BY_PAGE]
     return render(request, "blog/index.html", {"post_list": post_list})
+
+
+class ProfileDetailView(DetailView):
+    template_name = 'blog/profile.html'
+    model = get_user_model()
+    slug_url_kwarg = 'username'
+    slug_field = 'username'
+    context_object_name = 'profile'
 
 
 def post_detail(request, post_id):
