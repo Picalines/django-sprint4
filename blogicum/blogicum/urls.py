ec7892ef3af3811605055a242ff375d4b26bf906
picalines <picalines0@gmail.com>
2025-09-28 22:47:03 +0300

    feat: add built-in auth paths

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
 ]
