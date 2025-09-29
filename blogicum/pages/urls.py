0b47c69ccd3bf514b35fbf584de5d0ab32ffa548
picalines <picalines0@gmail.com>
2025-09-29 19:27:56 +0300

    fix: temp logout view

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import AboutPage, RulesPage
+from .views import AboutPage, LogoutPage, RulesPage
 
 app_name = "pages"
 
 urlpatterns = [
     path("about/", AboutPage.as_view(), name="about"),
     path("rules/", RulesPage.as_view(), name="rules"),
+    path('auth/logout/', LogoutPage.as_view(), name='logout'),
 ]
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -10,6 +10,18 @@ class RulesPage(TemplateView):
     template_name = 'pages/rules.html'
 
 
+class LogoutPage(TemplateView):
+    """
+    Временный view до https://github.com/yandex-praktikum/django-sprint4/pull/1
+
+    Вкратце: тесты ищут *первую* форму во всей разметке. Из-за этого нельзя
+    добавить форму logout в header.html - нужно чтобы она жила на отдельной
+    странице. Это и делает этот view
+    """
+
+    template_name = 'pages/logout.html'
+
+
 def not_found_page(request, exception):
     return render(request, 'pages/404.html', status=404)
 
